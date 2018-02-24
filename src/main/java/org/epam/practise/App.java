343571b89d4025a5f38b291bb31e3ea46af57330
github-sindhu <sindhura0101@gmail.com>
2018-02-24 15:22:59 +0530

    first commit

---
+++ src/main/java/org/epam/practise/App.java
@@ -0,0 +1,13 @@
+package org.epam.practise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
